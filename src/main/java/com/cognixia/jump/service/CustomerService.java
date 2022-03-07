79648faf3d26f767ac5cfad4ea84a371878edd11
unknown <awm21@pitt.edu>
2022-03-07 12:46:03 -0500

    added service and repository files

--- src/main/java/com/cognixia/jump/service/CustomerService.java
+++ src/main/java/com/cognixia/jump/service/CustomerService.java
@@ -1,8 +1,14 @@
 package com.cognixia.jump.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cognixia.jump.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
 
+	@Autowired
+	CustomerRepository repo;
+	
 }
